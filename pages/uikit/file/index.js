16390fd1f88daf026f4835294733dc05b6f1caf2
habubey <habubey@outlook.com>
2023-04-05 14:47:27 +0300

    Upload file api refactor

--- pages/uikit/file/index.js
+++ pages/uikit/file/index.js
@@ -1,28 +1,24 @@
 import React, { useRef } from 'react';
-
 import { FileUpload } from 'primereact/fileupload';
+import { Toast } from 'primereact/toast';
 
 const FileDemo = () => {
     const toast = useRef(null);
 
     const onUpload = () => {
-        toast.current.show({
-            severity: 'info',
-            summary: 'Success',
-            detail: 'File Uploaded',
-            life: 3000
-        });
+        toast.current.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
     };
 
     return (
         <div className="grid">
+            <Toast ref={toast}></Toast>
             <div className="col-12">
                 <div className="card">
                     <h5>Advanced</h5>
-                    <FileUpload name="demo[]" url={`/upload.php`} onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
+                    <FileUpload name="demo[]" url="/api/upload" onUpload={onUpload} multiple accept="image/*" maxFileSize={1000000} />
 
                     <h5>Basic</h5>
-                    <FileUpload mode="basic" name="demo[]" url={`/upload.php`} accept="image/*" maxFileSize={1000000} onUpload={onUpload} />
+                    <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" maxFileSize={1000000} onUpload={onUpload} />
                 </div>
             </div>
         </div>
